a50937ea383c7c07b411e54be9527111a19434b9
yuveex <alvinsetiawan2003@gmail.com>
2023-10-10 21:54:40 +0700

    Home Page UI Update

--- cerbung-project/src/app/home/home.page.ts
+++ cerbung-project/src/app/home/home.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  genrelist = ["Action", "Horror", "Comedy", "Politics"];
+
   constructor() { }
 
   ngOnInit() {
