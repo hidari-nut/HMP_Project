733cb0ba798887d8b5cace7085cdbc80e1488c2e
yuveex <alvinsetiawan2003@gmail.com>
2023-10-10 17:07:53 +0700

    Base Update
    Changed the pages to a more familiar form

--- cerbung-project/src/app/app-routing.module.ts
+++ cerbung-project/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
+
 ];
 @NgModule({
   imports: [
