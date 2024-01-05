58675d79ca13d125b068497cbc043c1429ac65db
hidari-nut <madevis02@gmail.com>
2024-01-05 18:40:18 +0700

    new update

--- cerbung-project/src/app/app.component.ts
+++ cerbung-project/src/app/app.component.ts
@@ -8,16 +8,35 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   showTabBar: boolean = false;
-  eligiblePages: string[] = ['/home', '/read', '/create1', '/create2', '/create3', '/following', '/preference', '/user', '/accountview', '/notification', '/approvals'];
+  eligiblePages: { path: string; title: string }[] = [
+    { path: '/home', title: 'Cerbung' },
+    { path: '/read', title: 'Read' },
+    { path: '/create1', title: 'Create Cerbung' },
+    { path: '/create2', title: 'Create Cerbung' },
+    { path: '/create3', title: 'Create Cerbung' },
+    { path: '/following', title: 'Followed Cerbungs' },
+    { path: '/user', title: 'Users' },
+    { path: '/accountview', title: 'Account Details' },
+    { path: '/notification', title: 'Notifications' },
+    { path: '/approvals', title: 'Approval Page' },
+    { path: '/preference', title: 'Preferences' },
+
+  ];
+  pageTitle: string = ''; 
 
   constructor(private router: Router) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         const currentRoute = event.url;
-        this.showTabBar = this.eligiblePages.includes(currentRoute);
+        const currentPage = this.eligiblePages.find(
+          (page) => page.path === currentRoute
+        );
+        this.pageTitle = currentPage ? currentPage.title : 'Cerbung';
+        this.showTabBar = this.eligiblePages.map(page => page.path).includes(currentRoute);
       }
     });
   }
-  
-  ngOnInit() {  }
+
+  ngOnInit() {}
 }
+
